d213e379ea6fffdfc124140d74157b884af475f9
Giuseppe Caponetto <giuseppegi.caponetto@studenti.unicam.it>
2015-05-14 16:16:28 +0200

    example updated

--- examples/modulator_example_1.rb
+++ examples/modulator_example_1.rb
@@ -10,5 +10,16 @@ class A
   end
 end
 
+class B < A
+  def initialize
+    @var=1
+  end
+  def method_of_class_b
+    puts "method_of_class_b was called."
+  end
+end
+
 a = A.new
 a.example_module_method
+b = B.new
+b.example_module_method
