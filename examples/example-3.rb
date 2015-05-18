bd96019ed9eecdd3371972ded206c25b0db066bc
Giuseppe Caponetto <giuseppegi.caponetto@studenti.unicam.it>
2015-05-18 09:47:30 +0200

    building up functionalities

---
+++ examples/example-3.rb
@@ -0,0 +1,8 @@
+require_relative '../lib/flexo.rb'
+
+my_flexo_obj = Flexo.new({:velocity=>127, :midi_channel=>10})
+
+my_flexo_obj[:velocity]=99
+
+p my_flexo_obj.inspect
+p my_flexo_obj.class
--- lib/flexo.rb
+++ lib/flexo.rb
@@ -13,11 +13,23 @@ class Flexo
     end
   end
   
+  #restituisce il valore della tabella avente per chiave il simbolo "name"
+  #equivale a farsi restituire il valore con la sintassi flexo_obj.name
+  
+  def [](name)
+    @table[name.to_sym]
+  end
+  
+  #imposta il valore della tabella con chiave "name" e valore "value"
+  def []=(name, value)
+    @table[name] = value
+  end
+  
   #definisce nella metaclasse i metodi per leggere e scrivere nella tabella
   #TODO le stringhe devono essere formattate per poter essere accessibili mediante
   #la notazione :xxx , possibile risolverlo con name.downcase.gsub(/\s+/, "_").to_sym che produce "stringa esempio" #=> stringa_esempio
   #si ricorda che l'hash viene valutato partendo dall'ultimo simbolo fino a risalire al primo.
-  
+  private
   def make_new_member(name)
     name = name.to_sym
     if !respond_to?(name)
