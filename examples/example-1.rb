4a4915b0e6eb7b32f91e52598f24638bb1401c01
Giuseppe Caponetto <giuseppegi.caponetto@studenti.unicam.it>
2015-05-18 22:28:54 +0200

    resetting repo

--- examples/example-1.rb
+++
@@ -1,7 +0,0 @@
-require_relative '../lib/flexo.rb'
-
-my_flexo_obj = Flexo.new
-my_flexo_obj.make_new_member(:size)
-my_flexo_obj.size=20
-
-p my_flexo_obj.size
--- examples/example-6.rb
+++
@@ -1,5 +0,0 @@
-require_relative '../lib/flexo.rb'
-
-my_flexo_obj = Flexo.new("Bomb", {:velocity=>127, :midi_channel=>10}, {:shout => Proc.new{puts 'ahhhhhhh!!!'}})
-
-p my_flexo_obj
