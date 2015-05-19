382a1f30b8d41011da5b890c04940811c4b137f4
Giuseppe Caponetto <giuseppegi.caponetto@studenti.unicam.it>
2015-05-19 17:37:43 +0200

    major changes

--- examples/example-1.rb
+++ examples/example-1.rb
@@ -4,10 +4,13 @@ require_relative '../lib/pattern-builder.rb'
 
 pattern = PatternBuilder::Pattern.new "Hip Hop", "A groovy pattern suited for 85 bpm", 16 do 
   channel "kicks" do 
-    fill_with 1
+    
   end
   channel "snares"do
-  fill_with 2
+    
+  end
+  channel "conhas" do
+    
   end
 end
 
--- lib/pattern-builder.rb
+++ lib/pattern-builder.rb
@@ -1,20 +1,37 @@
 module PatternBuilder
+  class Step
+    attr_accessor :hash
+    def initialize(state=0, velocity=0, probability=0)
+      self.hash = {:state=>state, :velocity=>velocity, :probability=>probability}
+    end
+    def to_custom_string
+      output = String.new
+      hash.each {|key, value| output<< "[#{value}]" }
+      output
+    end
+  end
   
   class Channel
     attr_accessor :name, :array
     def initialize(name, steps)
       self.name = name
       self.array = Array.new(steps, nil)
+      populate
     end
     
     def to_custom_string
       output = name+": "
       array.each do |element|
-        output << "[#{element.to_s}]"
+        output << "[#{element.to_custom_string}]"
       end
       output 
     end
     private
+    def populate
+      array.each_index do |index|
+        array[index] = Step.new
+      end
+    end
     def fill_with(elem)
       array.each_index do |index|
         array[index] = elem
